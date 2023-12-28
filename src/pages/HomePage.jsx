77096ae66d7f152b827f7d3cb4bd390cd1ee976d
Gustavo Lennyng <51210591+GustavoLennyng@users.noreply.github.com>
2023-12-27 21:29:21 -0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import RegisterAccount from "../components/RegisterAccount";
 import { auth } from "../utils/Firebase";
 import { isOfficial } from "../utils/FirebaseFunctions";
 import TrafficArt from "/src/assets/traffic-art.png";
 import Navbar from "/src/components/Navbar";
+import Logo from "/src/assets/logo.png"; // Importe a imagem da logo
+
 const HomePage = () => {
   const navigate = useNavigate();
+  const [User, setUser] = useState(null);
+  const [Official, setOfficial] = useState(false);
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user && !isOfficial(user.uid)) {
@@ -14,23 +19,36 @@ const HomePage = () => {
       } else if (user && isOfficial(user.uid)) {
         navigate("/official-dashboard");
       }
+      setUser(user);
+      isOfficial(user?.uid).then((res) => {
+        setOfficial(res);
+      });
     });
-  }, []);
+  }, [navigate]);
+
   return (
     <div className="HomePage">
       <Navbar />
-      <div className="HomeContainer grid grid-cols-1 lg:grid-cols-2 items-center px-5 lg:px-20">
+      <div className="HomeContainer grid grid-cols-1 lg:grid-cols-5 items-center px-5 lg:px-20 gap-1">
+        {/* Adicione a imagem da logo aqui */}
+        <img className="logo h-50 lg:h-63 w-72" src={Logo} alt="Logo" />
+
+        {/* Imagem TrafficArt */}
         <img
-          className="TrafficArt hidden lg:block "
+          className="TrafficArt hidden lg:block w-1/1 ml-1"
           src={TrafficArt}
-          alt=""
+          alt="Traffic Art"
         />
-        <div>
-          <h3 className="slogan  lg:mt-0 leading-normal font-bold text-center text-base lg:text-[2rem]">
+
+        {/* Conteúdo do lado das imagens */}
+        <div className="lg:col-span-3"> {/* Ajuste o número conforme necessário */}
+          <h3 className="slogan lg:mt-9 leading-normal font-bold text-center text-base lg:text-[2rem]">
             ESTAMOS AQUI PARA TE OUVIR
           </h3>
           <RegisterAccount />
         </div>
+
+        {/* Adicione outros elementos aqui, se necessário */}
       </div>
     </div>
   );
